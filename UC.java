6502e4898f9e7c9473245e43e65bf8cab92a4412
OjashviSingh <ojashvisingh365@gmail.com>
2023-01-19 19:03:00 +0530

    Add Part Time Employee & Wage

--- UC.java
+++ UC.java
@@ -1,19 +1,25 @@
 import java.util.Random;
 public class UC {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        Random rd = new Random();
-        Boolean a = rd.nextBoolean();
-        int  workingHour;
-        if (a){
-            System.out.println("Employee is present.");
-            workingHour = 8;
+        public static void main(String[] args) {
+                int perHourSalary = 20;
+                int workingHour = 0;
+                int totalSalary = 0;
+
+                Random randomNum = new Random();
+                int x = randomNum.nextInt(3);
+                if (x == 0) {
+                    System.out.println("Employee is absent");
+                    workingHour = 0;
+                } else if (x == 1) {
+                    System.out.println("Employee is present");
+                    workingHour = 8;
+                } else {
+                    System.out.println("Employee is working as part time.");
+                    workingHour = 8;
+                }
+
+                int salary = perHourSalary * workingHour;
+                totalSalary = totalSalary + salary;
+                System.out.println("Employee has earned " + totalSalary +" in a day.");
+                }
         }
-        else {
-            System.out.println("Employee is absent.");
-            workingHour = 0;
-        }
-        int salary = workingHour * 20;
-        System.out.println("Employee Daily Wage is: "+salary);
-    }
-}
