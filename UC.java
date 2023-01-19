d3fa0fa78170c59d66a637b7c727546acf581eda
OjashviSingh <ojashvisingh365@gmail.com>
2023-01-19 19:24:45 +0530

    Solving using

--- UC.java
+++ UC.java
@@ -1,25 +1,28 @@
 import java.util.Random;
 public class UC {
-        public static void main(String[] args) {
-                int perHourSalary = 20;
-                int workingHour = 0;
-                int totalSalary = 0;
+    public static void main(String[] args) {
 
-                Random randomNum = new Random();
-                int x = randomNum.nextInt(3);
-                if (x == 0) {
-                    System.out.println("Employee is absent");
-                    workingHour = 0;
-                } else if (x == 1) {
-                    System.out.println("Employee is present");
-                    workingHour = 8;
-                } else {
-                    System.out.println("Employee is working as part time.");
-                    workingHour = 8;
-                }
+        int perHourSalary = 20;
+        int workingHour = 0;
+        int totalSalary = 0;
 
-                int salary = perHourSalary * workingHour;
-                totalSalary = totalSalary + salary;
-                System.out.println("Employee has earned " + totalSalary +" in a day.");
-                }
+        Random randomNum = new Random();
+        int x = randomNum.nextInt(3);
+
+        switch(x){
+            case 0: System.out.println("Employee is absent");
+                workingHour = 0;
+                break;
+            case 1: System.out.println("Employee is present");
+                workingHour = 8;
+                break;
+            case 2: System.out.println("Employee is working as part time.");
+                workingHour = 8;
+                break;
         }
+
+        int salary = perHourSalary * workingHour;
+        totalSalary = totalSalary + salary;
+        System.out.println("Employee has earned " + totalSalary +" in a day.");
+    }
+}
