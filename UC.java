9cf5d478a922d69bc66fb34bf382498b3d3b42ab
OjashviSingh <ojashvisingh365@gmail.com>
2023-01-19 21:54:48 +0530

    Calculating Wage for a Month

--- UC.java
+++ UC.java
@@ -1,13 +1,26 @@
 import java.util.Random;
 public class UC {
     public static void main(String[] args) {
-        Random randomNum = new Random();
-        int x = randomNum.nextInt(2);
-        if (x == 0){
-            System.out.println("Employee is present.");
+        int perHourSalary = 20;
+        int workingHour = 0;
+        int totalSalary = 0;
+
+        for (int i = 1; i <= 20; i++) {
+            Random randomNum = new Random();
+            int x = randomNum.nextInt(3);
+            if (x == 0) {
+                System.out.println("Employee is absent");
+                workingHour = 0;
+            } else if (x == 1) {
+                System.out.println("Employee is present");
+                workingHour = 8;
+            } else {
+                System.out.println("Employee is working as part time.");
+                workingHour = 8;
+            }
+            int salary = perHourSalary * workingHour;
+            totalSalary = totalSalary + salary;
         }
-        else {
-            System.out.println("Employee is absent.");
-        }
+        System.out.println("Employee has earned a total of " + totalSalary + " in a month.");
     }
 }
