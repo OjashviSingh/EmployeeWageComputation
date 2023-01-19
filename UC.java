cb5d39dabe9ac058f71431431165a9c6db1009d7
OjashviSingh <107572304+OjashviSingh@users.noreply.github.com>
2023-01-19 23:34:58 +0530

    Update UC.java

--- UC.java
+++ UC.java
@@ -1,19 +1,29 @@
 import java.util.Random;
 public class UC {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-        Random rd = new Random();
-        Boolean a = rd.nextBoolean();
-        int  workingHour;
-        if (a){
-            System.out.println("Employee is present.");
-            workingHour = 8;
+        int perHourSalary = 20;
+        int workingHour = 0;
+        int totalSalary = 0;
+        int days = 1;
+
+        while (days <= 20 || workingHour <= 100) {
+            Random randomNum = new Random();
+            int x = randomNum.nextInt(3);
+            if (x == 0) {
+                System.out.println("Employee is absent");
+                workingHour += 0;
+            } else if (x == 1) {
+                System.out.println("Employee is present");
+                workingHour += 8;
+            } else {
+                System.out.println("Employee is working as part time.");
+                workingHour += 8;
+            }
+            int salary = perHourSalary * workingHour;
+            totalSalary = totalSalary + salary;
+            days++;
         }
-        else {
-            System.out.println("Employee is absent.");
-            workingHour = 0;
-        }
-        int salary = workingHour * 20;
-        System.out.println("Employee Daily Wage is: "+salary);
+        System.out.println("Employee has earned a total of " + totalSalary + " in a month.");
+        System.out.println(workingHour);
     }
 }
