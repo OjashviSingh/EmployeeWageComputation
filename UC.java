1ad4650b9f650637ab3e07d2dc0ca8f7bfb600a3
OjashviSingh <ojashvisingh365@gmail.com>
2023-01-19 14:01:57 +0530

    UC2: Added

--- UC.java
+++ UC.java
@@ -1,14 +1,21 @@
+package EmployeeWage;
+
 import java.util.Random;
 public class UC {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch.");
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
         Random rd = new Random();
         Boolean a = rd.nextBoolean();
+        int  workingHour;
         if (a){
             System.out.println("Employee is present.");
+            workingHour = 8;
         }
         else {
             System.out.println("Employee is absent.");
+            workingHour = 0;
         }
+        int salary = workingHour * 20;
+        System.out.println("Employee Daily Wage is: "+salary);
     }
 }
